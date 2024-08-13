76f1ab96dea6b77a13ddc51dffcf1a41c6adb813
hm-montagel <maria.montagel@web.de>
2024-08-13 12:00:26 +0900

    weitere Eingabefelder im Formular zum Erstellen eines ToDo-Elements

--- test_anwendung/src/ToDoForm.js
+++ test_anwendung/src/ToDoForm.js
@@ -1,37 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './ToDoForm.css'
 
-function ToDoForm({ addTodo, todos}) {
-  const [input, setInput] = useState('');
-
-  useEffect(() => { console.log("nnnn")}, []);
-
-  const handleChange = (e) => {
-    setInput(e.target.value);
-  };
+function ToDoForm({ addTodo, todos }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [duration, setDuration] = useState('');
+  const [importance, setImportance] = useState(1);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     addTodo({
-        id: todos.length,
-      text: input
+      id: todos.length,
+      title: title,
+      description: description,
+      duration: duration,
+      importance: importance,
     });
 
-    setInput('');
+    setTitle('');
+    setDescription('');
+    setDuration('');
+    setImportance(1);
   };
 
   return (
-    <form className='todoForm' onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Neue Aufgabe"
-        value={input}
-        onChange={handleChange}
-        name="text"
-        className="todo-input"
-      />
-      <button className="todo-button">Hinzufügen</button>
-    </form>
+      <form className="todo-form" onSubmit={handleSubmit}>
+        <div>
+          <label>Titel</label>
+          <input
+            type="text"
+            placeholder="Aufgabentitel"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+            className="todo-input"
+          />
+        </div>
+        <div>
+          <label>Beschreibung</label>
+          <textarea
+            placeholder="Aufgabenbeschreibung"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+            className="todo-input"
+          />
+        </div>
+        <div>
+          <label>Dauer (in Stunden)  </label>
+          <input
+            type="number"
+            placeholder="Dauer"
+            min="0"
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+            required
+            className="todo-input"
+          />
+        </div>
+        <div>
+          <label>Wichtigkeit (1-10)  </label>
+          <input
+            type="number"
+            min="1"
+            max="10"
+            value={importance}
+            onChange={(e) => setImportance(e.target.value)}
+            required
+            className="todo-input"
+          />
+        </div>
+        <button className="todo-button">Hinzufügen</button>
+      </form>
   );
 }
 
